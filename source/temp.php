f9344baab5a68343db54779352fa5580cdad9d4e
James Lei <j14370@gmail.com>
2011-01-14 12:46:53 +0800

    新增一template
    Signed-off-by: James Lei <j14370@gmail.com>

---
+++ source/temp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>Travel Journal</title>
+    <link rel="stylesheet" href="stylesheet/style.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="stylesheet/reset.css" type="text/css" media="screen" charset="utf-8">
+</head>
+<body>
+    <header>
+        <h1>旅行者日誌</h1>
+        <p>Travel Journal</p>
+    </header>
+    <nav>login | nav1 | nav2</nav>
+    <article>
+        hello
+    </article>
+    <footer><p>資管三 史繼中 唐百博 李永裕</p></footer>
+</body>
+</html>
