34ff3b644c2b07f8eaa2b176699ae4bb25c0d68a
James Lei <j14370@gmail.com>
2011-01-14 14:02:41 +0800

    增加各class的內容
    Signed-off-by: James Lei <j14370@gmail.com>

--- source/class.php
+++ source/class.php
@@ -1,6 +1,36 @@
 <?php
 class Account{
-  public $username, $aid;
-  private $pass;
+  public $name, $id, $account_id;
+  private $password;
+}
+class Traveller{
+  public $id, $name, $gender, $birthday, $address;
+}
+class Group{
+  public $id, $name, $description, $account_id;
+}
+class Trip{
+  public $id, $type, $time, $status, $group_id;
+}
+class Location{
+  public $id, $name, $trip_id, $city_id, $next, $next_traffic;
+}
+class Day{
+  public $id, $date, $next;
+}
+class Schedule{
+  public $id, $time, $expanse_id, $next;
+}
+class Favorite{
+  public $id, $name, $time, $type, $note, $location_id;
+}
+class city{
+  public $name, $country;
+}
+class Country{
+  public $name;
+}
+class Shelter{
+  public $id, $name, $info, $location_id;
 }
 ?>
