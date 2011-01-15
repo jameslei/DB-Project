c35f0549966dbcb57a182ca3cefef49e874c2552
JamesLei <j14370@gmail.com>
2011-01-16 01:39:17 +0800

    首頁
    Signed-off-by: JamesLei <j14370@gmail.com>

--- source/index.php
+++ source/index.php
@@ -1,9 +1,7 @@
 <?php require_once "template/header.php"; ?>
 
-<?php
+<?php $user = Account::find($_SESSION['aid']); ?>
 
-    $user = Account::find($_SESSION['aid']);
-
-?>
+<p class="welcome">你好! <?php echo $user->traveller->name; ?>, 你可以按此<a href="logout.php">登出</a>.</p>
 
 <?php require_once "template/footer.php"; ?>
