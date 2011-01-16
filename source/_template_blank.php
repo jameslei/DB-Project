6ff6849f5c1fd082b7c0503ee09263818098cba3
JamesLei <j14370@gmail.com>
2011-01-16 12:30:08 +0800

    用來使用的template
    Signed-off-by: JamesLei <j14370@gmail.com>

---
+++ source/_template_blank.php
@@ -0,0 +1,5 @@
+<?php require_once "template/blank.php"; ?>
+
+
+
+<?php mysql_close($db_server);?>
---
+++ source/_template_full.php
@@ -0,0 +1,5 @@
+<?php require_once "template/header.php"; ?>
+
+<!-- PUT CONTENT HERE -->
+
+<?php require_once "template/footer.php"; ?>
