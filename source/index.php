0c96c38b1deb32f3004aff7657044fcb7db95822
Joshua <joshuascc@gmail.com>
2011-01-20 02:27:58 +0800

    minor changes
    Signed-off-by: Joshua <joshuascc@gmail.com>

--- source/index.php
+++ source/index.php
@@ -124,7 +124,7 @@
         }
         ?>
         <ul>
-            <li><a href="#">新增</a></li>
+            <!-- <li><a href="newfave.php">新增</a></li> -->
             <li><a href=<?php echo "fav.php?uid=$uid"?>>更多</a></li>
         </ul> 
     </div>
